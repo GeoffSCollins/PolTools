9aa0bc06c55669fd7d845e61ac249776bb7a796f
GeoffSCollins <39243170+GeoffSCollins@users.noreply.github.com>
2021-03-16 10:42:56 -0500

    Add a make.py file to download hg38.fa and source the bash completion script
    Could have used a Makefile, but I stuck to what I like -- python! :snake:

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(name='GC_bioinfo', version='1.0', packages=find_packages())
+setup(name='GC_bioinfo', author='Geoff Collins', version='1.0', packages=find_packages())
